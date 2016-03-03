75cd3413d4697e2a9aa4a4df1cc7fa888513e23c
Praveen Selvam <praveen.selvam@gmail.com>
2016-03-03 19:23:09 +0530

    Fixed issues with dependencies

--- components/core.js
+++ components/core.js
@@ -1,9 +1,13 @@
-var bootstrapCSS = require("!style!css!sass!./bootstrap/bootstrap.scss");
+const tetherCSS = require('!style!css!../node_modules/tether/dist/css/tether.min.css');
+const bootstrapCSS = require("!style!css!sass!./bootstrap/bootstrap.scss");
 
 const React = require('react');
 const ReactDOM = require('react-dom');
+
+$ = window.jQuery = require('../node_modules/jquery/dist/jquery.min.js');
+window.Tether = require('../node_modules/tether/dist/js/tether.min.js');
 const bootstrap = require('../node_modules/bootstrap/dist/js/bootstrap.min.js');
 
 var subtitle = <span>The front end framework for an Indix experience</span>;
 
-ReactDOM.render(subtitle, document.getElementById('subtitle'));
+ReactDOM.render(subtitle, $('#subtitle')[0]);
