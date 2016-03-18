d7215f13361968d2f963463203de0bbcd5a23a7b
Praveen Selvam <praveen.selvam@gmail.com>
2016-03-18 11:19:10 +0530

    Adding a favicon

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.locals.ENV_DEVELOPMENT = env == 'development';
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
