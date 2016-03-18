358b511c2dd1420540e87bde9174cdd947ef5314
Praveen Selvam <praveen.selvam@gmail.com>
2016-03-18 10:03:52 +0530

    Adding a build script for CI

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+rm -rf node_modules
+rm -rf dist && mkdir -p dist
+
+source ~/.nvm/nvm.sh
+set -x
+nvm use 5.0.0
+npm version "$GO_PIPELINE_LABEL"
+
+npm install
+
+npm run build
+
+tar --exclude=test --exclude=.git --exclude=dist --exclude=node_modules -czf dist/the-vision.tgz .
