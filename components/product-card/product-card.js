ebfd9d4771322b92bcc1491d5ed591c8e4d1bbf0
Praveen Selvam <praveen.selvam@gmail.com>
2016-03-18 15:05:14 +0530

    Revert "Merging from middleman branch"
    This reverts commit 0fb130d957a193c7bde50054b86b9d36737b5070.

---
+++ components/product-card/product-card.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import ProductCardStyles from '!style!css!sass!./product-card.scss';
+
+class ProductCard extends React.Component{
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div className="card card-block product-card">
+        <div className="image">
+          <img src={this.props.image} alt={this.props.image} className="img-rounded" />
+        </div>
+        <div className="info">
+          <div className="title-wrap">
+            <h6 className="title">{this.props.title}</h6>
+          </div>
+          <h6 className="price">{this.props.price}</h6>
+        </div>
+        <a href={this.props.link} className="btn btn-primary" target="_blank">Buy Now</a>
+      </div>
+    )
+  }
+}
+
+export default ProductCard;
