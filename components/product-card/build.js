8c177b5f3d456d2a03082cc3073d7afd4c31821d
Bharath ツ <bharathraja@live.com>
2016-04-02 17:56:46 +0530

    taking out the css loader out of product card component

---
+++ components/product-card/build.js
@@ -0,0 +1,4 @@
+import '!style!css!sass!./product-card.scss';
+import ProductCard from './product-card'
+
+export default ProductCard;
--- components/product-card/product-card.js
+++ components/product-card/product-card.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import ProductCardStyles from '!style!css!sass!./product-card.scss';
-
 class ProductCard extends React.Component{
 
   constructor(props) {
