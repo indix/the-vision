ebd1089b1f12721c6a775f0238fd5dd86f09686f
Bharath ツ <bharathraja@live.com>
2016-06-09 14:51:30 +0530

    minor change in publish script

--- publish-npm.sh
+++ publish-npm.sh
@@ -6,7 +6,7 @@ rm -rf node_modules
 source ~/.nvm/nvm.sh
 set -x
 nvm use
-npm version "0.8.$GO_PIPELINE_LABEL"
-# npm version $(node -e "console.log(require('./package.json').version)")".$GO_PIPELINE_LABEL"
+# npm version "0.8.$GO_PIPELINE_LABEL"
+npm version $(node -e "console.log(require('./package.json').version)")".$GO_PIPELINE_LABEL"
 
 npm publish
